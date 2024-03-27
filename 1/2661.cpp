4a00d01939e9ad9948b23719ae3c57ca1088ecf4
Igor <igorlfs@ufmg.br>
2024-03-27 13:32:52 -0300

    1/2661: use ll

--- 1/2661.cpp
+++ 1/2661.cpp
@@ -2,19 +2,20 @@
 
 using namespace std;
 
+using ll = long long;
+
 int main() {
-    cin.tie(nullptr);
-    long n, m;
+    ll n, m;
     cin >> n;
     m = n;
-    set<int> factors;
+    set<ll> factors;
     if (n % 2 == 0) {
         factors.insert(2);
         while (n % 2 == 0) {
             n = n / 2;
         }
     }
-    for (int i = 3; i <= sqrt(m); i += 2) {
+    for (ll i = 3; i <= (ll)sqrt(m); i += 2) {
         if (n % i == 0) {
             factors.insert(i);
             while (n % i == 0) {
@@ -23,7 +24,7 @@ int main() {
         }
         if (n == 1) break;
     }
-    if (n != 1) factors.insert((int)n);
-    cout << int(pow(2, factors.size())) - factors.size() - 1 << endl;
+    if (n != 1) factors.insert((ll)n);
+    cout << ll(pow(2, factors.size())) - factors.size() - 1 << endl;
     return 0;
 }
